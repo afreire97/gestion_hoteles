56594ea90cd66d40d8b65ce0144b35203e6c04d6
Aarón Freire <freiresousa97@gmail.com>
2024-05-22 13:26:18 +0200

    comercial.sql actualizado

--- bill_maker/insertarEstados.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO estados_presupuestos (EST_nombre, created_at, updated_at) VALUES
-('en preparacion', NOW(), NOW()),
-('en curso', NOW(), NOW()),
-('aceptado', NOW(), NOW()),
-('cancelado', NOW(), NOW());
--- comercial.sql
+++ comercial.sql
@@ -5,17 +5,22 @@ INSERT INTO productos(PRO_nombre, PRO_tipo, PRO_cifra, created_at, updated_at) V
 ('Checkin', 'IN', 200, now(), now()),
 ('Anual', 'DE', 10, now(), now()),
 ('Comision', 'CO', 20, now(), now()),
-('Habitacion1','IN' ,30, now(), now()),
-('Habitacion2', 'IN',21 , now(), now()),
-('Habitacion3','IN' ,18, now(), now()),
-('Habitacion4', 'IN',16.50, now(), now()),
-('Habitacion5', 'IN',15, now(), now()),
-('Habitacion6','IN' ,13.50, now(), now()),
-('Habitacion7','IN' ,12, now(), now());
+('1','HAB' ,30, now(), now()),
+('2', 'HAB',21 , now(), now()),
+('3','HAB' ,18, now(), now()),
+('4', 'HAB',16.50, now(), now()),
+('5', 'HAB',15, now(), now()),
+('6','HAB' ,13.50, now(), now()),
+('7','HAB' ,12, now(), now());
+
+INSERT INTO estados_presupuestos (EST_nombre, created_at, updated_at) VALUES
+('en preparacion', NOW(), NOW()),
+('en curso', NOW(), NOW()),
+('aceptado', NOW(), NOW()),
+('cancelado', NOW(), NOW());
 
 
-
-INSERT INTO clientes_tipos (tipo,created_at,updated_at) VALUES 
+INSERT INTO clientes_tipos (CLI_tipo,created_at,updated_at) VALUES 
 ('Hotel', now(), now()),
 ('Motel', now(), now()),
 ('Apartamento', now(), now());
