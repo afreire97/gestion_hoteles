83b410d3cd295afcaf6dc08ec6cdbc6807aed0c7
Aarón Freire <freiresousa97@gmail.com>
2024-05-23 10:25:56 +0200

    cambios

--- bill_maker/config/app.php
+++ bill_maker/config/app.php
@@ -83,6 +83,12 @@ return [
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'locales' => [
+        'en' => 'English',
+        'es' => 'Español',
+        'fr' => 'Français', // Añade francés
+        'de' => 'Deutsch', // Añade alemán
+    ],
 
     /*
     |--------------------------------------------------------------------------
